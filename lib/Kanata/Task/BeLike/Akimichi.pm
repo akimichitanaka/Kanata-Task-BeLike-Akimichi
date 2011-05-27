dc4ff65b034c0a3d2204e6785c4abee44c1a3421
Akimichi Tanaka <akimichi@thekanata.jp>
2011-05-27 10:10:23 +0900

    initial commit

---
+++ lib/Kanata/Task/BeLike/Akimichi.pm
@@ -0,0 +1,32 @@
+package Kanata::Task::BeLike::Akimichi;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Kanata::Task::BeLike::Akimichi -
+
+=head1 SYNOPSIS
+
+  use Kanata::Task::BeLike::Akimichi;
+
+=head1 DESCRIPTION
+
+Kanata::Task::BeLike::Akimichi is
+
+=head1 AUTHOR
+
+akimichi E<lt>akimichi@thekanata.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
